cac1e77a1ce562276c3e27a53166eeab4abb95ba
Mahendra <mahendrabrowny@gmail.com>
2022-04-25 13:49:01 +0530

    folder structured and dependencis are intalled

---
+++ src/chakra/theme.ts
@@ -0,0 +1,14 @@
+// 1. Import the extendTheme function
+import { extendTheme } from "@chakra-ui/react";
+
+// 2. Extend the theme to include custom colors, fonts, etc
+export const theme = {
+  colors: {
+    brand: {
+      100: "#fff3c00",
+    },
+  },
+  fonts: {
+    body: "Open Sans, sans-serif",
+  },
+};
