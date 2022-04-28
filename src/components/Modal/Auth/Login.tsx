15ec460b5a51f7af7c303a47e51536371f3eb6c5
Mahendra <mahendrabrowny@gmail.com>
2022-04-28 23:35:10 +0530

    signup component is left

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -1,9 +1,13 @@
-import { Button, Input } from "@chakra-ui/react";
+import { Button, Flex, Input, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "../../../atoms/authModalAtom";
 
 type LoginProps = {};
 
 const Login: React.FC<LoginProps> = () => {
+  const setAuthModalState = useSetRecoilState(authModalState);
+
   const [loginForm, setLoginForm] = useState({
     email: "",
     password: "",
@@ -29,19 +33,55 @@ const Login: React.FC<LoginProps> = () => {
         onChange={() => {
           onChange;
         }}
+        fontSize="10pt"
+        _placeholder={{ color: "gray.500" }}
+        _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
       />
       <Input
         required
         name="password"
         placeholder="password"
         type="password"
+        mb={2}
         onChange={() => {
           onChange;
+        }}
+        fontSize="10pt"
+        _placeholder={{ color: "gray.500" }}
+        _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
         }}
       />
       <Button type="submit" width="100%" height="36px" mt={2} mb={2}>
         Log In
       </Button>
+      <Flex fontSize="8pt" justifyContent="center">
+        <Text>New here? </Text>
+        <Text
+          onClick={() =>
+            setAuthModalState((prev) => ({
+              ...prev,
+              view: "signup",
+            }))
+          }
+          ml={1}
+          color="blue.500"
+          fontWeight={700}
+          cursor="pointer"
+        >
+          SIGN UP
+        </Text>
+      </Flex>
     </form>
   );
 };
