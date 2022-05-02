7977aa4ca7d049d60c50fc80d1903456ffbcfcf9
Mahendra <mahendrabrowny@gmail.com>
2022-05-02 20:25:50 +0530

    Google Login feature done

--- src/components/Modal/Auth/Login.tsx
+++ src/components/Modal/Auth/Login.tsx
@@ -1,19 +1,28 @@
 import { Button, Flex, Input, Text } from "@chakra-ui/react";
 import React, { useState } from "react";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
 import { useSetRecoilState } from "recoil";
 import { authModalState } from "../../../atoms/authModalAtom";
+import { auth } from "../../../firebase/clientApp";
+import { FIREBASE_ERRORS } from "../../../firebase/errors";
 
 type LoginProps = {};
 
 const Login: React.FC<LoginProps> = () => {
   const setAuthModalState = useSetRecoilState(authModalState);
 
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
   const [loginForm, setLoginForm] = useState({
     email: "",
     password: "",
   });
 
-  const onSubmit = () => {};
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    signInWithEmailAndPassword(loginForm.email, loginForm.password);
+  };
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -31,9 +40,7 @@ const Login: React.FC<LoginProps> = () => {
         placeholder="email"
         type="email"
         mb={2}
-        onChange={() => {
-          onChange;
-        }}
+        onChange={onChange}
         fontSize="10pt"
         _placeholder={{ color: "gray.500" }}
         _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
@@ -51,9 +58,7 @@ const Login: React.FC<LoginProps> = () => {
         placeholder="password"
         type="password"
         mb={2}
-        onChange={() => {
-          onChange;
-        }}
+        onChange={onChange}
         fontSize="10pt"
         _placeholder={{ color: "gray.500" }}
         _hover={{ bg: "white", border: "1px solid", borderColor: "blue.500" }}
@@ -65,6 +70,11 @@ const Login: React.FC<LoginProps> = () => {
         }}
         bg="gray.50"
       />
+
+      <Text textAlign="center" color="red" fontSize="10pt">
+        {FIREBASE_ERRORS[error?.message as keyof typeof FIREBASE_ERRORS]}
+      </Text>
+
       <Button type="submit" width="100%" height="36px" mt={2} mb={2}>
         Log In
       </Button>
--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,10 +1,19 @@
-import { Button, Flex, Image } from "@chakra-ui/react";
+import { Button, Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { auth } from "../../../firebase/clientApp";
 
 const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+
   return (
-    <Flex direction="column" width="100%" mb={1}>
-      <Button variant="oauth" mb={2}>
+    <Flex
+      direction="column"
+      width="100%"
+      mb={1}
+      onClick={() => signInWithGoogle()}
+    >
+      <Button variant="oauth" mb={2} isLoading={loading}>
         <Image
           src="/images/googlelogo.png"
           alt="googleLogo"
@@ -13,6 +22,7 @@ const OAuthButtons: React.FC = () => {
         />
         Continue with Google
       </Button>
+      {error && <Text>{error.message}</Text>}
     </Flex>
   );
 };
