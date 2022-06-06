8b319a96af6530e116df42d91331551d634605ea
Mahendra <mahendrabrowny@gmail.com>
2022-06-06 22:00:10 +0530

    server side rendering for community page

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,9 +1,35 @@
+import { doc, getDoc } from "firebase/firestore";
+import { GetServerSideProps } from "next";
 import React from "react";
+import { firestore } from "../../../firebase/clientApp";
 
-type CommunityPageProps = {};
+type CommunityPageProps = {
+  communityData: Community;
+};
 
-const CommunityPage: React.FC<CommunityPageProps> = () => {
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
   return <div>CommunityPage</div>;
 };
+
+//Server Side Rendering
+export async function getServerSideProps(context: GetServerSideProps) {
+  //get community data and pass it to client side
+  try {
+    const communityDocRef = doc(
+      firestore,
+      "communities",
+      context.query.communityId as string
+    );
+    const communityDoc = await getDoc(communityDocRef);
+
+    return {
+      props: {
+        communityData: communityDoc.data(),
+      },
+    };
+  } catch (error) {
+    console.log("getServerSide Error: ", error);
+  }
+}
 
 export default CommunityPage;
