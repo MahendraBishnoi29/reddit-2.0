40044f2cdbfe5908f80bc8bc7c60b67b26e5d624
Mahendra <mahendrabrowny@gmail.com>
2022-06-19 09:33:30 +0530

    facing server side rendering issue

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,21 +1,26 @@
 import { doc, getDoc } from "firebase/firestore";
-import { GetServerSideProps } from "next";
+import { GetServerSidePropsContext } from "next";
 import React from "react";
 import { Community } from "../../../atoms/communitiesAtom";
 import { firestore } from "../../../firebase/clientApp";
 import safeJsonStringify from "safe-json-stringify";
+import CommunityNotFound from "../../../components/Community/NotFound";
 
 type CommunityPageProps = {
   communityData: Community;
 };
 
 const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  if (!communityData) {
+    return <CommunityNotFound />;
+  }
+
   return <div>Welcome To: {communityData.id}</div>;
 };
 
 //Server Side Rendering
-export async function getServerSideProps(context: GetServerSideProps) {
-  //get community data and pass it to client side
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // get community data and pass it to client
   try {
     const communityDocRef = doc(
       firestore,
@@ -26,13 +31,16 @@ export async function getServerSideProps(context: GetServerSideProps) {
 
     return {
       props: {
-        communityData: JSON.parse(
-          safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
-        ),
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+            )
+          : "",
       },
     };
   } catch (error) {
-    console.log("getServerSide Error: ", error);
+    // Could add error page here
+    console.log("getServerSideProps error", error);
   }
 }
 
