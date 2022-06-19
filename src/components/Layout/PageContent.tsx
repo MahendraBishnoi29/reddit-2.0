2d809b0ebb971ac04c2cf0dc0266de5639728ff0
Mahendra <mahendrabrowny@gmail.com>
2022-06-19 10:24:25 +0530

    Community page content component structure created

---
+++ src/components/Layout/PageContent.tsx
@@ -0,0 +1,40 @@
+import { Flex } from "@chakra-ui/react";
+import React, { ReactNode } from "react";
+
+type PageContentProps = {
+  children: ReactNode[];
+};
+
+const PageContent: React.FC<PageContentProps> = ({ children }) => {
+  return (
+    <Flex justify="center" p="16px 0px" border="1px solid red">
+      <Flex
+        border="1px solid green"
+        width="95%"
+        justify="center"
+        maxWidth="860px"
+      >
+        {/* LHS */}
+        <Flex
+          border="1px solid blue"
+          direction="column"
+          width={{ base: "100%", md: "65%" }}
+          mr={{ base: 0, md: 6 }}
+        >
+          {children && children[0]}
+        </Flex>
+
+        {/* RHS */}
+        <Flex
+          border="1px solid orange"
+          direction="column"
+          display={{ base: "none", md: "flex" }}
+          flexGrow={1}
+        >
+          {children && children[1]}
+        </Flex>
+      </Flex>
+    </Flex>
+  );
+};
+export default PageContent;
--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -6,6 +6,7 @@ import { firestore } from "../../../firebase/clientApp";
 import safeJsonStringify from "safe-json-stringify";
 import CommunityNotFound from "../../../components/Community/NotFound";
 import Header from "../../../components/Community/Header";
+import PageContent from "../../../components/Layout/PageContent";
 
 type CommunityPageProps = {
   communityData: Community;
@@ -19,6 +20,19 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
   return (
     <>
       <Header communityData={communityData} />
+      <PageContent>
+        <>
+          <div>LHS</div>
+          <div>LHS</div>
+          <div>LHS</div>
+          <div>LHS</div>
+          <div>LHS</div>
+          <div>LHS</div>
+        </>
+        <>
+          <div>RHS</div>
+        </>
+      </PageContent>
     </>
   );
 };
