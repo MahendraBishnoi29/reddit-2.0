584b85d4b01059589bf1d7e06752e6114812a3cd
Mahendra <mahendrabrowny@gmail.com>
2022-07-04 15:39:53 +0530

    join or joined function for community is completed

--- src/components/Community/Header.tsx
+++ src/components/Community/Header.tsx
@@ -2,13 +2,17 @@ import { Box, Button, Flex, Icon, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { FaReddit } from "react-icons/fa";
 import { Community } from "../../atoms/communitiesAtom";
+import useCommunityData from "../../hooks/useCommunityData";
 
 type HeaderProps = {
   communityData: Community;
 };
 
 const Header: React.FC<HeaderProps> = ({ communityData }) => {
-  const isJoined = false;
+  const { communityStateValue, onJoinOrLeaveCommunity } = useCommunityData();
+  const isJoined = !!communityStateValue.mySnippets.find(
+    (item) => item.communityId === communityData?.id
+  );
 
   return (
     <Flex direction="column" width="100%" height="164px">
@@ -44,6 +48,7 @@ const Header: React.FC<HeaderProps> = ({ communityData }) => {
               height="30px"
               pr={6}
               pl={6}
+              onClick={() => onJoinOrLeaveCommunity(communityData, isJoined)}
             >
               {isJoined ? "Joined" : "Join"}
             </Button>
