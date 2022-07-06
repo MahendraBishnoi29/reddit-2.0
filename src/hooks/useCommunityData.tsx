c1ea3cd820dfee72a68e5b15fd576d8a3c93bc31
Mahendra <mahendrabrowny@gmail.com>
2022-07-06 23:10:11 +0530

    community snippets created

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -1,18 +1,64 @@
-import React from "react";
-import { useRecoilState } from "recoil";
-import { communityState } from "../atoms/communitiesAtom";
+import { collection, getDocs } from "firebase/firestore";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { authModalState } from "../atoms/authModalAtom";
+import { Community, communityState } from "../atoms/communitiesAtom";
+import { auth, firestore } from "../firebase/clientApp";
 
 const useCommunityData = () => {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
   const [communityStateValue, setCommunityStateValue] =
     useRecoilState(communityState);
+  const setAuthModalState = useSetRecoilState(authModalState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const onJoinOrLeaveCommunity = (
+    communityData: Community,
+    isJoined: boolean
+  ) => {
+    // is the user signed in?
+    // if not => open auth modal
+    if (isJoined) {
+      // open modal
+      leaveCommunity(communityData?.id);
+      return;
+    }
+
+    joinCommunity(communityData);
+  };
+
+  const getMySnippets = async () => {
+    setLoading(true);
+    try {
+      // get users snippets
+      const snippetDocs = await getDocs(
+        collection(firestore, `users/${user?.uid}/communitySnippets`)
+      );
+
+      const snippets = snippetDocs.docs.map((doc) => ({ ...doc.data() }));
+
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        mySnippets: snippets as CommunitySnippet[],
+        snippetsFetched: true,
+      }));
+    } catch (error: any) {
+      console.log("getMySnippets error", error);
+      setError(error.message);
+    }
+    setLoading(false);
+  };
 
   const joinCommunity = () => {};
   const leaveCommunity = () => {};
 
   return {
     communityStateValue,
-    joinCommunity,
-    leaveCommunity,
+    onJoinOrLeaveCommunity,
   };
 };
 
