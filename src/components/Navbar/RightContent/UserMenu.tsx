b8a429824fa179cc96ae8b090d31d75516eb30d7
Mahendra <mahendrabrowny@gmail.com>
2022-07-20 13:54:45 +0530

    reset the join community button when user logout ✔

--- src/components/Navbar/RightContent/UserMenu.tsx
+++ src/components/Navbar/RightContent/UserMenu.tsx
@@ -17,17 +17,20 @@ import { VscAccount } from "react-icons/vsc";
 import { IoSparkles } from "react-icons/io5";
 import { CgProfile } from "react-icons/cg";
 import { MdOutlineLogin } from "react-icons/md";
-import { useSetRecoilState } from "recoil";
+import { useResetRecoilState, useSetRecoilState } from "recoil";
 import { authModalState } from "../../../atoms/authModalAtom";
 import { auth } from "../../../firebase/clientApp";
+import { communityState } from "../../../atoms/communitiesAtom";
 
 type UserMenuProps = { user?: User | null };
 
 const UserMenu: React.FC<UserMenuProps> = ({ user }) => {
+  const resetCommunityState = useResetRecoilState(communityState);
   const setAuthModalState = useSetRecoilState(authModalState);
 
   const logOut = async () => {
     await signOut(auth);
+    resetCommunityState();
   };
 
   return (
