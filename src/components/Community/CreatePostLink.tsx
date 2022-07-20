a1a7bb37d9d7ac06007099d5686f97db65a60f18
Mahendra <mahendrabrowny@gmail.com>
2022-07-20 21:51:26 +0530

    post component for creating a post 🚀

---
+++ src/components/Community/CreatePostLink.tsx
@@ -0,0 +1,77 @@
+import { Flex, Icon, Input } from "@chakra-ui/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { BsLink45Deg } from "react-icons/bs";
+import { FaReddit } from "react-icons/fa";
+import { IoImageOutline } from "react-icons/io5";
+import { useSetRecoilState } from "recoil";
+import { authModalState } from "../../atoms/authModalAtom";
+import { auth } from "../../firebase/clientApp";
+import useDirectory from "../../hooks/useDirectory";
+
+const CreatePostLink: React.FC = () => {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+  const setAuthModalState = useSetRecoilState(authModalState);
+
+  const { toggleMenuOpen } = useDirectory();
+  const onClick = () => {
+    // Could check for user to open auth modal before redirecting to submit
+    if (!user) {
+      setAuthModalState({ open: true, view: "login" });
+    }
+    const { communityId } = router.query;
+    router.push(`/r/${communityId}/submit`);
+    console.log(communityId);
+  };
+
+  return (
+    <Flex
+      justify="space-evenly"
+      align="center"
+      bg="white"
+      height="56px"
+      borderRadius={4}
+      border="1px solid"
+      borderColor="gray.300"
+      p={2}
+      mb={4}
+    >
+      <Icon as={FaReddit} fontSize={36} color="gray.300" mr={4} />
+      <Input
+        placeholder="Create Post"
+        fontSize="10pt"
+        _placeholder={{ color: "gray.500" }}
+        _hover={{
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        _focus={{
+          outline: "none",
+          bg: "white",
+          border: "1px solid",
+          borderColor: "blue.500",
+        }}
+        bg="gray.50"
+        borderColor="gray.200"
+        height="36px"
+        borderRadius={4}
+        mr={4}
+        onClick={onClick}
+      />
+      <Icon
+        as={IoImageOutline}
+        fontSize={24}
+        mr={4}
+        color="gray.400"
+        cursor="pointer"
+      />
+      <Icon as={BsLink45Deg} fontSize={24} color="gray.400" cursor="pointer" />
+    </Flex>
+  );
+};
+
+export default CreatePostLink;
