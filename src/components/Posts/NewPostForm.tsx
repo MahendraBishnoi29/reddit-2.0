c33fe6a42338dff233469863a552d881712b72a3
Mahendra <mahendrabrowny@gmail.com>
2022-07-21 22:46:46 +0530

    syntax mistakes are solved in NewPostForm cmponent

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,4 +1,4 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Icon, TabPanels } from "@chakra-ui/react";
 import React, { useEffect, useRef, useState } from "react";
 import { User } from "firebase/auth";
 
@@ -8,47 +8,55 @@ import { BsLink45Deg, BsMic } from "react-icons/bs";
 import { IoDocumentText, IoImageOutline } from "react-icons/io5";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { useRecoilState, useSetRecoilState } from "recoil";
+import TabItem from "./TabItem";
 
 type NewPostFormProps = {};
 
-function NewPostForm({}: NewPostFormProps) {
-  const formTabs = [
-    {
-      title: "Post",
-      icon: IoDocumentText,
-    },
-    {
-      title: "Images & Video",
-      icon: IoImageOutline,
-    },
-    {
-      title: "Link",
-      icon: BsLink45Deg,
-    },
-    {
-      title: "Poll",
-      icon: BiPoll,
-    },
-    {
-      title: "Talk",
-      icon: BsMic,
-    },
-  ];
+const formTabs: TabItem[] = [
+  {
+    title: "Post",
+    icon: IoDocumentText,
+  },
+  {
+    title: "Images & Video",
+    icon: IoImageOutline,
+  },
+  {
+    title: "Link",
+    icon: BsLink45Deg,
+  },
+  {
+    title: "Poll",
+    icon: BiPoll,
+  },
+  {
+    title: "Talk",
+    icon: BsMic,
+  },
+];
 
-  export type TabItem = {
-    title: string;
-    icon: typeof Icon.arguments;
-  };
+export type TabItem = {
+  title: string;
+  icon: typeof Icon.arguments;
+};
+
+const NewPostForm: React.FC<NewPostFormProps> = () => {
+  const [selectedTab, setSelectedTab] = useState(formTabs[0].title);
 
   return (
     <Flex direction="column" bg="white" borderRadius={4} mt={2}>
       <Flex width="100%">
-        {formTabs.map((item) => (
-          <></>
+        {formTabs.map((item, k) => (
+          <TabItem
+            key={k}
+            item={item}
+            selected={item.title === selectedTab}
+            setSelectedTab={setSelectedTab}
+          />
         ))}
       </Flex>
     </Flex>
   );
-}
+};
 
 export default NewPostForm;
