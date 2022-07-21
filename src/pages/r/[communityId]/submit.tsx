7f1d577541c35a69284a6da891b6eaf6964a2597
Mahendra <mahendrabrowny@gmail.com>
2022-07-21 22:48:14 +0530

    submit tab UI created but have some issues 😣

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,7 +1,17 @@
+import { Box, Text } from "@chakra-ui/react";
 import React from "react";
+import PageContent from "../../../components/Layout/PageContent";
 
 const SubmitPostPage: React.FC = () => {
-  return <div>Submit Page</div>;
+  return (
+    <PageContent>
+      <>
+        <Box p="16px 0px" border="1px solid" borderColor="white">
+          <Text>Create A Submit Form</Text>
+        </Box>
+      </>
+    </PageContent>
+  );
 };
 
 export default SubmitPostPage;
