0fa87ed6b174d8455f5d1c96ec00399dbd1bff93
Mahendra <mahendrabrowny@gmail.com>
2022-07-22 17:01:40 +0530

    submit error function solved 🎉

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,15 +1,18 @@
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 import PageContent from "../../../components/Layout/PageContent";
+import NewPostForm from "../../../components/Posts/NewPostForm";
 
 const SubmitPostPage: React.FC = () => {
   return (
     <PageContent>
       <>
-        <Box p="16px 0px" border="1px solid" borderColor="white">
+        <Box p="16px 0px" borderBottom="1px solid" borderColor="white">
           <Text>Create A Submit Form</Text>
         </Box>
+        <NewPostForm />
       </>
+      <></>
     </PageContent>
   );
 };
