0155da5921d69f964be8d2164d3a9e4e5146a228
Mahendra <mahendrabrowny@gmail.com>
2022-07-23 13:20:16 +0530

    naming mistake in file

---
+++ src/atoms/communitiesAtom.ts
@@ -0,0 +1,30 @@
+import { Timestamp } from "firebase/firestore";
+import { atom } from "recoil";
+
+export interface Community {
+  id: string;
+  creatorId: string;
+  numberOfMembers: number;
+  privacyType: "public" | "restricted" | "private";
+  createdAt?: Timestamp;
+  imageURL?: string;
+}
+
+export interface CommunitySnippet {
+  communityId: string;
+  isModerator?: boolean;
+  imageURL?: string;
+}
+
+interface CommunityState {
+  mySnippets: CommunitySnippet[];
+}
+
+const defaultCommunityState: CommunityState = {
+  mySnippets: [],
+};
+
+export const communityState = atom<CommunityState>({
+  key: "communitiesState",
+  default: defaultCommunityState,
+});
