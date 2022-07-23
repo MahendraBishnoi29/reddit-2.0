20aa6678d39075d0a7e877ee0981a2839821263b
Mahendra <mahendrabrowny@gmail.com>
2022-07-23 13:21:44 +0530

    Image uploading with firebase is done

---
+++ src/components/Posts/PostForm/ImageUpload.tsx
@@ -0,0 +1,69 @@
+import { Flex, Button, Image, Stack } from "@chakra-ui/react";
+import React, { useRef } from "react";
+
+type ImageUploadProps = {
+  selectedFile?: string;
+  onSelectImage: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  setSelectedTab: (value: string) => void;
+  setSelectedFile: (value: string) => void;
+};
+
+const ImageUpload: React.FC<ImageUploadProps> = ({
+  selectedFile,
+  onSelectImage,
+  setSelectedFile,
+  setSelectedTab,
+}) => {
+  const selectedFileRef = useRef<HTMLInputElement>(null);
+  return (
+    <Flex justify="center" align="center" width="100%" direction="column">
+      {selectedFile ? (
+        <>
+          <Image
+            src={selectedFile}
+            maxWidth="400px"
+            maxHeight="400px"
+            alt="Selected File by user"
+          />
+          <Stack direction="row" mt={4}>
+            <Button height="28px" onClick={() => setSelectedTab("Post")}>
+              Back to Post
+            </Button>
+            <Button
+              variant="outline"
+              height="28px"
+              onClick={() => setSelectedFile("")}
+            >
+              Remove
+            </Button>
+          </Stack>
+        </>
+      ) : (
+        <Flex
+          justify="center"
+          align="center"
+          p={20}
+          border="1px dashed"
+          borderColor="gray.200"
+          width="100%"
+          borderRadius={4}
+        >
+          <Button
+            variant="outline"
+            height="28px"
+            onClick={() => selectedFileRef.current?.click()}
+          >
+            Upload
+          </Button>
+          <input
+            ref={selectedFileRef}
+            hidden
+            type="file"
+            onChange={onSelectImage}
+          />
+        </Flex>
+      )}
+    </Flex>
+  );
+};
+export default ImageUpload;
