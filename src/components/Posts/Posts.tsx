db635d55e429b9d2c4fa30691056e6685fbb3787
Mahendra <mahendrabrowny@gmail.com>
2022-07-26 16:15:42 +0530

    toDo : show posts to the frontend

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { Community } from "../../atoms/communitiesAtom";
+import { firestore } from "../../firebase/clientApp";
 
 type PostsProps = {
   communityData: Community;
 };
 
-const Posts: React.FC<PostsProps> = () => {
+const Posts: React.FC<PostsProps> = ({ communityData }) => {
+  const [loading, setLoading] = useState(false);
+
+  const getPosts = async () => {
+    try {
+      const postQuery = query(
+        collection(firestore, "posts"),
+        where("communityId", "==", communityData?.id),
+        orderBy("createdAt", "desc")
+      );
+      const postDocs = await getDocs(postQuery);
+      const posts = postDocs.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+      console.log(posts);
+    } catch (error: any) {
+      console.log("get Post error", error.message);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return <div>Have A Good Coding....</div>;
 };
 
