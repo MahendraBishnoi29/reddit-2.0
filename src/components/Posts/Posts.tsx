e0491c473554d2794a896d725fcac92ab864569b
Mahendra <mahendrabrowny@gmail.com>
2022-07-26 15:43:27 +0530

    Post component to fetching the posts from firebase

---
+++ src/components/Posts/Posts.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Community } from "../../atoms/communitiesAtom";
+
+type PostsProps = {
+  communityData: Community;
+};
+
+const Posts: React.FC<PostsProps> = () => {
+  return <div>Have A Good Coding....</div>;
+};
+
+export default Posts;
--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -8,6 +8,7 @@ import CommunityNotFound from "../../../components/Community/NotFound";
 import Header from "../../../components/Community/Header";
 import PageContent from "../../../components/Layout/PageContent";
 import CreatePostLink from "../../../components/Community/CreatePostLink";
+import Posts from "../../../components/Posts/Posts";
 
 type CommunityPageProps = {
   communityData: Community;
@@ -24,6 +25,7 @@ const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
       <PageContent>
         <>
           <CreatePostLink />
+          <Posts communityData={communityData} />
         </>
         <>
           <div>RHS</div>
