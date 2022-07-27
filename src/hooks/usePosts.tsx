75835ab2afa4f512fdaa83c755b2c54ea4342015
Mahendra <mahendrabrowny@gmail.com>
2022-07-27 13:49:54 +0530

    Delete post feature done

--- src/hooks/usePosts.tsx
+++ src/hooks/usePosts.tsx
@@ -1,6 +1,9 @@
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
 import React from "react";
 import { useRecoilState } from "recoil";
-import { postState } from "../atoms/postsAtom";
+import { Post, postState } from "../atoms/postsAtom";
+import { firestore, storage } from "../firebase/clientApp";
 
 const usePosts = () => {
   const [postStateValue, setPostStateValue] = useRecoilState(postState);
@@ -9,7 +12,29 @@ const usePosts = () => {
 
   const onSelectPost = async () => {};
 
-  const onDeletePost = async () => {};
+  const onDeletePost = async (post: Post): Promise<boolean> => {
+    try {
+      //check if there is an image
+
+      if (post.imageURL) {
+        const imageRef = ref(storage, `posts/${post?.id}/image`);
+        await deleteObject(imageRef);
+      }
+      //delete the post document
+      const postDocRef = doc(firestore, "posts", post.id!);
+      await deleteDoc(postDocRef);
+      //update recoil state
+      setPostStateValue((prev) => ({
+        ...prev,
+        posts: prev.posts.filter((item) => item.id !== post.id),
+      }));
+
+      return true;
+    } catch (error: any) {
+      console.log(error.message);
+      return false;
+    }
+  };
 
   return {
     postStateValue,
