00eb5e2d76b63aaf58c1018541ee948a6572f942
Mahendra <mahendrabrowny@gmail.com>
2022-07-28 20:17:58 +0530

    unnecessary console.log removed

---
+++ src/components/Posts/Comments/Comments.tsx
@@ -0,0 +1,117 @@
+import { Box, Flex, useToast } from "@chakra-ui/react";
+import { User } from "firebase/auth";
+import {
+  collection,
+  doc,
+  increment,
+  serverTimestamp,
+  Timestamp,
+  writeBatch,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { Post } from "../../../atoms/postsAtom";
+import { firestore } from "../../../firebase/clientApp";
+import CommentInput from "./CommentInput";
+
+type CommentsProps = {
+  user: User;
+  selectedPost: Post | null;
+  communityId: string;
+};
+
+export type Comment = {
+  id: string;
+  creatorId: string;
+  creatorDisplayText: string;
+  communityId: string;
+  postId: string;
+  postTitle: string;
+  text: string;
+  createdAt: Timestamp;
+};
+
+const Comments: React.FC<CommentsProps> = ({
+  user,
+  selectedPost,
+  communityId,
+}) => {
+  const [commentText, setCommentText] = useState("");
+  const [comments, setComments] = useState<Comment[]>([]);
+  const [fetchLoading, setFetchLoading] = useState(false);
+  const [createLoading, setCreateLoading] = useState(false);
+  const toast = useToast();
+
+  const onCreateComment = async (commentText: string) => {
+    // Create a comment document
+    setCreateLoading(true);
+    try {
+      const batch = writeBatch(firestore);
+
+      const commentDocRef = doc(collection(firestore, "comments"));
+      const newComment: Comment = {
+        id: commentDocRef.id,
+        creatorId: user.uid,
+        creatorDisplayText: user.email!.split("@")[0],
+        communityId,
+        postId: selectedPost?.id!,
+        postTitle: selectedPost?.title!,
+        text: commentText,
+        createdAt: serverTimestamp() as Timestamp,
+      };
+
+      batch.set(commentDocRef, newComment);
+      // Update post numberOfComments
+      const postDocRef = doc(firestore, "posts", selectedPost?.id!);
+      batch.update(postDocRef, {
+        numberOfComments: increment(1),
+      });
+
+      await batch.commit();
+      toast({
+        title: "Comment Added ✔",
+        status: "success",
+        duration: 3000,
+        isClosable: true,
+        position: "top-right",
+      });
+
+      setCommentText("");
+      setComments((prev) => [newComment, ...prev]);
+    } catch (error: any) {
+      console.log(error.message);
+    }
+    setCreateLoading(false);
+    // Update client recoil state
+  };
+
+  const onDeleteComment = async (comments: any) => {};
+
+  const getPostComments = async () => {};
+
+  useEffect(() => {
+    getPostComments();
+  }, []);
+
+  return (
+    <Box bg="white" borderRadius="0px 0px 4px 4px" p={2}>
+      <Flex
+        direction="column"
+        pl={10}
+        pr={4}
+        mb={6}
+        fontSize="10pt"
+        width="100%"
+      >
+        <CommentInput
+          commentText={commentText}
+          setCommentText={setCommentText}
+          user={user}
+          createLoading={createLoading}
+          onCreateComment={onCreateComment}
+        />
+      </Flex>
+    </Box>
+  );
+};
+
+export default Comments;
