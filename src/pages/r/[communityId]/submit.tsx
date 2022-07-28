092665b873a24def91bbc1da2593558f7b5ce6c2
Mahendra <mahendrabrowny@gmail.com>
2022-07-28 20:17:18 +0530

    Comment component is created

--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -1,15 +1,16 @@
 import { Box, Text } from "@chakra-ui/react";
 import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useRecoilValue } from "recoil";
-import { communityState } from "../../../atoms/communitiesAtom";
+import About from "../../../components/Community/About";
 import PageContent from "../../../components/Layout/PageContent";
 import NewPostForm from "../../../components/Posts/PostForm/NewPostForm";
 import { auth } from "../../../firebase/clientApp";
+import useCommunityData from "../../../hooks/useCommunityData";
 
 const SubmitPostPage: React.FC = () => {
   const [user] = useAuthState(auth);
-  const communityStateValue = useRecoilValue(communityState);
+  const { communityStateValue } = useCommunityData();
+
   return (
     <PageContent>
       <>
@@ -18,7 +19,11 @@ const SubmitPostPage: React.FC = () => {
         </Box>
         {user && <NewPostForm user={user} />}
       </>
-      <></>
+      <>
+        {communityStateValue.currentCommunity && (
+          <About communityData={communityStateValue.currentCommunity} />
+        )}
+      </>
     </PageContent>
   );
 };
