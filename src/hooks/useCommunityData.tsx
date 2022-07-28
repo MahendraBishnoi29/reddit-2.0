f4713b07cc8c36f1ce140ebbb8051284affeef43
Mahendra <mahendrabrowny@gmail.com>
2022-07-28 15:24:54 +0530

    PopUp modal & toast if user isn't logged in

--- src/hooks/useCommunityData.tsx
+++ src/hooks/useCommunityData.tsx
@@ -133,7 +133,13 @@ const useCommunityData = () => {
   };
 
   useEffect(() => {
-    if (!user) return;
+    if (!user) {
+      setCommunityStateValue((prev) => ({
+        ...prev,
+        mySnippets: [],
+      }));
+      return;
+    }
     getMySnippets();
   }, [user]);
 
